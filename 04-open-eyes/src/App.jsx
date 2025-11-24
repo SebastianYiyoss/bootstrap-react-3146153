92bf80124d7410dd7833bbfd7b592a21325dfc90
SebastianYiyoSs <xdjuasjuan@gmail.com>
2025-11-24 13:24:54 -0500

    OPEN EYES

---
+++ 04-open-eyes/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+    <header>
+      <div className='sec-1'>
+        <img src="../sources/Logo.png" alt="" style={{width: '20%'}} />
+        <h2>conoce la marca</h2>
+      </div>
+      <div className='sec-1'>
+        <img src="../sources/Abajo.svg" alt="" />
+        <h2>inicio</h2>
+        <img src="../sources/Arriba.svg" alt="" />
+      </div>
+    </header>
+
+
+
+    </>
+  )
+}
+
+export default App
